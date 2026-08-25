37482a442db742265a66706b189d5e95643c8487
TechnoDot <73961295+TechDudie@users.noreply.github.com>
2026-08-25 03:26:28 -0500

    feat: initial layout

---
+++ README.md
@@ -0,0 +1,4 @@
+# technodot
+
+who am i?<br>
+good question! let's find out together.
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '@fontsource/jetbrains-mono';
+	import '@sarap422/font-d-din/font-face.adjusted.css';
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
