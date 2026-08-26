db69661f278f96601613d650442bfa37b0fc339e
TechnoDot <73961295+TechDudie@users.noreply.github.com>
2026-08-26 17:29:39 -0500

    feat: socials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,8 @@
         alt="an shiraishi"
     />
 
-	<section
-		class="absolute top-[calc(50%+0rem)] right-[clamp(1.5rem,5vw,5rem)] left-[clamp(1.5rem,5vw,5rem)] z-10 flex max-w-xl -translate-y-1/2 flex-col gap-3 text-white"
-	>
-		<h1 id="hero-title" class="font-mono font-bold tracking-tighter text-4xl sm:text-6xl">technodot</h1>
+	<section class="absolute top-[calc(50%+0rem)] right-[clamp(1.5rem,5vw,5rem)] left-[clamp(1.5rem,5vw,5rem)] z-10 flex max-w-xl -translate-y-1/2 flex-col gap-3 text-white">
+		<h1 class="font-mono font-bold tracking-tighter text-4xl sm:text-6xl">technodot</h1>
 
 		<p class="max-w-lg font-content text-sm leading-6 text-white sm:text-base">
 			proud tsundere<br>
@@ -28,6 +26,41 @@
 			</a>
 		</div>
 	</section>
+
+	<section class="absolute bottom-2 right-3">
+		<p class="flex items-center justify-end gap-1 text-right text-sm text-white">
+			<svg class="size-3.5 shrink-0 fill-current" viewBox="0 0 16 16" aria-hidden="true">
+				<path
+					d="M6.766 11.328c-2.063-.25-3.516-1.734-3.516-3.656 0-.781.281-1.625.75-2.188-.203-.515-.172-1.609.063-2.062.625-.078 1.468.25 1.968.703.594-.187 1.219-.281 1.985-.281.765 0 1.39.094 1.953.265.484-.437 1.344-.765 1.969-.687.218.422.25 1.515.046 2.047.5.593.766 1.39.766 2.203 0 1.922-1.453 3.375-3.547 3.64.531.344.89 1.094.89 1.954v1.625c0 .468.391.734.86.547C13.781 14.359 16 11.53 16 8.03 16 3.61 12.406 0 7.984 0 3.563 0 0 3.61 0 8.031a7.88 7.88 0 0 0 5.172 7.422c.422.156.828-.125.828-.547v-1.25c-.219.094-.5.156-.75.156-1.031 0-1.64-.562-2.078-1.609-.172-.422-.36-.672-.719-.719-.187-.015-.25-.093-.25-.187 0-.188.313-.328.625-.328.453 0 .844.281 1.25.86.313.452.64.655 1.031.655s.641-.14 1-.5c.266-.265.47-.5.657-.656"
+				/>
+			</svg>
+			TechDudie
+		</p>
+		<p class="flex items-center justify-end gap-1 text-right text-sm text-white">
+			<svg class="size-3.75 shrink-0 fill-current" viewBox="0 0 59 44" aria-hidden="true">
+				<path
+					d="M37.1937 0C36.6265 1.0071 36.1172 2.04893 35.6541 3.11392C31.2553 2.45409 26.7754 2.45409 22.365 3.11392C21.9136 2.04893 21.3926 1.0071 20.8254 0C16.6928 0.70613 12.6644 1.94475 8.84436 3.69271C1.27372 14.9098 -0.775214 25.8374 0.243466 36.6146C4.67704 39.8906 9.6431 42.391 14.9333 43.9884C16.1256 42.391 17.179 40.6893 18.0819 38.9182C16.3687 38.2815 14.7133 37.4828 13.1274 36.5567C13.5442 36.2557 13.9493 35.9432 14.3429 35.6422C23.6384 40.0179 34.4039 40.0179 43.711 35.6422C44.1046 35.9663 44.5097 36.2789 44.9264 36.5567C43.3405 37.4943 41.6852 38.2815 39.9604 38.9298C40.8633 40.7009 41.9167 42.4025 43.109 44C48.3992 42.4025 53.3653 39.9137 57.7988 36.6377C59.0027 24.1358 55.7383 13.3007 49.1748 3.70429C45.3663 1.95633 41.3379 0.717706 37.2053 0.0231518L37.1937 0ZM19.3784 29.9816C16.5192 29.9816 14.1461 27.3886 14.1461 24.1821C14.1461 20.9755 16.4266 18.371 19.3669 18.371C22.3071 18.371 24.6455 20.9871 24.5992 24.1821C24.5529 27.377 22.2956 29.9816 19.3784 29.9816ZM38.6639 29.9816C35.7931 29.9816 33.4431 27.3886 33.4431 24.1821C33.4431 20.9755 35.7236 18.371 38.6639 18.371C41.6042 18.371 43.9309 20.9871 43.8846 24.1821C43.8383 27.377 41.581 29.9816 38.6639 29.9816Z"
+				/>
+			</svg>
+			technodot
+		</p>
+		<!-- <p class="flex items-center justify-end gap-1 text-right text-sm text-white">
+			<svg class="size-3 shrink-0 fill-current" viewBox="0 0 1200 1227" aria-hidden="true">
+				<path
+					d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
+				/>
+			</svg>
+			@TechnoDotX
+		</p> -->
+		<p class="flex items-center justify-end gap-1 text-right text-sm text-white">
+			<svg class="size-3.5 shrink-0 fill-current" viewBox="0 0 16 16" aria-hidden="true">
+				<path
+					d="M1.75 2h12.5c.966 0 1.75.784 1.75 1.75v8.5A1.75 1.75 0 0 1 14.25 14H1.75A1.75 1.75 0 0 1 0 12.25v-8.5C0 2.784.784 2 1.75 2ZM1.5 12.251c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25V5.809L8.38 9.397a.75.75 0 0 1-.76 0L1.5 5.809v6.442Zm13-8.181v-.32a.25.25 0 0 0-.25-.25H1.75a.25.25 0 0 0-.25.25v.32L8 7.88Z"
+				/>
+			</svg>
+			me@technodot.org
+		</p>
+	</section>
 </main>
 
 <main class="relative h-dvh w-full overflow-hidden">
@@ -37,11 +70,8 @@
         alt="an shiraishi"
     />
 
-	<section
-		class="absolute top-[calc(50%+0rem)] right-[clamp(1.5rem,5vw,5rem)] left-[clamp(1.5rem,5vw,5rem)] z-10 flex max-w-xl -translate-y-1/2 flex-col gap-3 text-black"
-		aria-labelledby="hero-title"
-	>
-		<h1 id="hero-title" class="font-mono font-bold tracking-tighter text-4xl sm:text-6xl">tuxops</h1>
+	<section class="absolute top-[calc(50%+0rem)] right-[clamp(1.5rem,5vw,5rem)] left-[clamp(1.5rem,5vw,5rem)] z-10 flex max-w-xl -translate-y-1/2 flex-col gap-3 text-black">
+		<h1 class="font-mono font-bold tracking-tighter text-4xl sm:text-6xl">tuxops</h1>
 
 		<p class="max-w-lg font-content text-sm leading-6 text-black sm:text-base">
 			ctf duo<br>
@@ -70,9 +100,7 @@
         alt="an shiraishi"
     />
 
-	<section
-		class="absolute top-[calc(50%+0rem)] right-[clamp(1.5rem,5vw,5rem)] left-[clamp(1.5rem,5vw,5rem)] z-10 flex max-w-xl -translate-y-1/2 flex-col gap-3 text-white"
-	>
+	<section class="absolute top-[calc(50%+0rem)] right-[clamp(1.5rem,5vw,5rem)] left-[clamp(1.5rem,5vw,5rem)] z-10 flex max-w-xl -translate-y-1/2 flex-col gap-3 text-white">
 	
 		<div class="py-6 px-7 bg-white/80 w-fit">
 			<p class="max-w-lg font-content text-sm leading-6 text-black sm:text-base">
@@ -86,7 +114,7 @@
 
 	<section class="absolute bottom-2 right-2">
 		<p class="text-black text-xs">
-			An Shiraishi © SEGA, Colorful Palette Inc., and Crypton Future Media, Inc. Usage is completely unofficial and independent.
+			An Shiraishi © SEGA, Colorful Palette Inc., and Crypton Future Media, Inc. Usage is unofficial and independent.
 		</p>
 	</section>
 </main>
